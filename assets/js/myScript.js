e679a7122f1fcb1cb89a1644ec4cdf85be0ac0e7
mccannEthan335 <112057069+mccannEthan335@users.noreply.github.com>
2022-10-03 07:02:09 -0500

    Update myScript.js

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -1,8 +1,10 @@
+
 document.querySelector('.menu-icon').addEventListener('click', openMenu);
 
 function openMenu() {
   document.getElementById('openmenu').style.display="block";
   document.querySelector('.settings').style.display="none";
+  document.getElementById('top').style.display='none';
 }
 
 document.querySelector('.load-more').addEventListener('click', loadMore);
@@ -16,6 +18,7 @@ document.querySelector(".exit").addEventListener('click', closeMenu);
 function closeMenu() {
   document.querySelector('#openmenu').style.display='none';
   document.querySelector('.settings').style.display='block';
+  document.getElementById('top').style.display='block'
 }
 
 document.querySelector('.settings').addEventListener('click', openSettings);
@@ -39,6 +42,7 @@ document.querySelector('#p-info').addEventListener('click', showInfo);
 function showInfo() {
   document.querySelector('.personal-info').style.display='block';
 
+
   document.querySelector('.settings-page').style.display='none';
 
 }
@@ -50,4 +54,20 @@ function accountInfo() {
   document.querySelector('.checking').style.display='block';
 
   document.querySelector('.home-page').style.display='none';
+}
+
+document.querySelector('.z').addEventListener('click', showDeposits);
+
+function showDeposits() {
+  document.querySelector('.deposit-list').style.display='block';
+
+}
+
+document.querySelector('.rh').addEventListener('click', goHome);
+
+function goHome() {
+  document.querySelector('.checking').style.display='none';
+  document.querySelector('.home-page').style.display='block';
+
+  document.querySelector('.deposit-list').style.display='none';
 }
